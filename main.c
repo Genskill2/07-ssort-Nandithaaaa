d0c2cc5970d2c80596ed4bb3f321fd79c87e91d1
Nandithaaaa <85017819+Nandithaaaa@users.noreply.github.com>
2021-07-01 21:00:44 +0530

    Secondc

--- main.c
+++ main.c
@@ -4,6 +4,30 @@
 
 void swap_max(int [], int, int );
 void ssort(int [], int);
+void swap_max(int arr[], int l, int position){
+  int flag = 0; 
+ int max = position;
+   for(int i = position ; i < l; i++){
+     if( arr[i] > arr[max] ){
+       max = i;
+      }
+    }
+     flag = arr[max];
+     arr[max] = arr[position];
+     arr[position] = flag;
+  
+   }
+
+void ssort(int arr[], int l){
+   for(int i = 0; i<l ; i++){
+     swap_max(arr,l,i);
+   }
+   for(int i = 0; i<l ; i++){
+   printf("| %i |",arr[i]);
+  }
+  printf("\n");
+}
+
 
 void print(int t[], int l) {
   for (int i = 0; i<l; i++) {
@@ -51,26 +75,3 @@ int main(void) {
   test_ssort();
   }
 
-void swap_max(int arr[], int l, int position){
-  int flag = 0; 
- int max = position;
-   for(int i = position ; i < l; i++){
-     if( arr[i] > arr[max] ){
-       max = i;
-      }
-    }
-     flag = arr[max];
-     arr[max] = arr[position];
-     arr[position] = flag;
-  
-   }
-
-void ssort(int arr[], int l){
-   for(int i = 0; i<l ; i++){
-     swap_max(arr,l,i);
-   }
-   for(int i = 0; i<l ; i++){
-   printf("| %i |",arr[i]);
-  }
-  printf("\n");
-}
